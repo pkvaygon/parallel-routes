8a3de81f583b86af269f250b0c4caaa254e65d7b
joelmiller1643@gmail.com <joelmiller1643@gmail.com>
2024-03-06 17:21:40 -0600

    asdasd

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const poppins = Poppins({style: ["normal"], weight: "500" });
+const poppins = Poppins({style: ["normal"], weight: "500", subsets: ["devanagari"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
